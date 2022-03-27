063f708677564bd4c1a9739b75f5235fed9572b4
junwei <jwlow@axrail.com>
2022-03-27 18:43:47 +0800

    change http to https

--- src/common/api.ts
+++ src/common/api.ts
@@ -1,11 +1,11 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance } from "axios";
 
 class Api {
   private static axiosInstance: AxiosInstance;
 
   static init() {
     this.axiosInstance = axios.create({
-      baseURL: 'http://api.themoviedb.org',
+      baseURL: "https://api.themoviedb.org",
     });
   }
 
